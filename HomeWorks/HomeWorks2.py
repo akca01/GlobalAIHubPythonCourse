8d72800644689a67e4a2b445e4ad69ef7f755b03
Mustafa Uğur Akça <80276406+akca01@users.noreply.github.com>
2021-03-10 21:58:06 +0300

    Add files via upload

---
+++ HomeWorks/HomeWorks2.py
@@ -0,0 +1,69 @@
+cv1 = {"name":"Mustafa" ,
+"surname":"Akça",
+"age": "25",
+"telephoneNo":"000000", 
+"e-mail":"akca@gmail.com",
+"adress":"Adana/Çukurova",
+"education":"University",
+"languages":"English",
+"job":"Urban And Regional Planner"
+}
+
+cv2 = {"name":"Eylem" ,
+"surname":"Gezer",
+"age": "22",
+"telephoneNo":"00254850000", 
+"e-mail":"gezer@gmail.com",
+"adress":"İstanbul/Bahçelievler",
+"education":"University",
+"languages":"German",
+"job":"Architect"
+}
+
+cv3 = {"name":"Yavuz" ,
+"surname":"Demir",
+"age": "36",
+"telephoneNo":"35625000", 
+"e-mail":"deimryavuz@gmail.com",
+"adress":"Isparta",
+"education":"University",
+"languages":"English",
+"job":"Teacher"
+}
+
+cv4 = {"name":"Gülsemin" ,
+"surname":"Naz",
+"age": "24",
+"telephoneNo":"5434534534", 
+"e-mail":"nazgül@gmail.com",
+"adress":"Çankırı",
+"education":"University",
+"languages":"German",
+"job":"Lawyer"
+}
+
+cv5 = {"name":"Kıvanç" ,
+"surname":"Tozlu",
+"age": "40",
+"telephoneNo":"2545555", 
+"e-mail":"kıvanctzlu@gmail.com",
+"adress":"Manisa/Selendi",
+"education":"University",
+"languages":"English",
+"job":"Civil Engineer"
+}
+
+for i in cv1:
+   print(i,": " ,cv1[i])
+print("********************")
+for i in cv2:
+   print(i,": " ,cv2[i])   
+print("********************")
+for i in cv1:
+   print(i,": " ,cv3[i])
+print("********************")
+for i in cv1:
+   print(i,": " ,cv4[i])   
+print("********************")
+for i in cv1:
+   print(i,": " ,cv5[i])
